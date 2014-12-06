9910f628e614366b37b7e3ef187ab24d4b794312
wang <wolf@wangdeMacBook-Pro.local>
2014-12-06 16:30:58 +0800

    Initial Commit

---
+++ cxlg/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  cxlg
+//
+//  Created by wang on 14-12-6.
+//  Copyright (c) 2014年 wang. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
